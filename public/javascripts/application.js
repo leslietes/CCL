0cebd454b854f1162db66499d7bcfeeab3c1f03d
leslie <leslie@ltanengsy.(none)>
2010-10-24 15:39:51 +0800

    added sample slider for home page

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,10 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 jQuery(document).ready(function(){
+	$('#s3slider').s3Slider({
+		timeOut: 2000
+	});
+	
 	$('#properties').dataTable();
 	$('#inquiries').dataTable();
 	
