1d9fb94fcc63e2c498871ab6638e4e97c86ebb09
leslie <leslie@ltanengsy.(none)>
2010-11-06 19:32:28 +0800

    set properties dropdown to select "All" by default

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,11 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 jQuery(document).ready(function(){
-	$('#properties').dataTable();
+	$('#properties').dataTable({
+		"aLengthMenu": [[-1, 10, 25, 50], ["All", 10, 25, 50]],
+		"iDisplayLength": "-1"
+	});
 	$('#inquiries').dataTable();
+	$('#subscriptions').dataTable();
 	$('#gallery a').lightBox();
 });
