9ed054985e8112ca803bed181d0f2e108d60ddf7
leslie <leslie@ltanengsy.(none)>
2010-11-17 23:23:38 +0800

    edit methods for optimization

--- app/models/property.rb
+++ app/models/property.rb
@@ -36,18 +36,32 @@ class Property < ActiveRecord::Base
     [[''],['Condominium'],['Condotel']]
   end
   
-  # include hidden
+  # include hidden - index page logged in user
   def self.show_all
-    Property.find(:all, :order => "name ASC")
+    Property.find(:all, :select => "name, permalink, location, target_completion_date, developer_id, completed, 
+                  studio, one_bedroom, two_bedroom, three_bedroom, penthouse, loft, 
+                  studio_size, one_bedroom_size, two_bedroom_size, three_bedroom_size, penthouse_size, loft_size, 
+                  studio_price, one_bedroom_price, two_bedroom_price, three_bedroom_price, penthouse_price, loft_price,
+                  studio_monthly, one_bedroom_monthly, two_bedroom_monthly, three_bedroom_monthly, penthouse_monthly, loft_monthly",
+                  :order => "name ASC", :include => :developer)
   end
   
-  # do not include hidden
+  # do not include hidden - index page
   def self.show_all_visible
-    Property.find(:all, :conditions => ["hidden = ?", false], :order => "name ASC")
+    Property.find(:all,  :select => "name, permalink, location, target_completion_date, developer_id, completed, 
+                  studio, one_bedroom, two_bedroom, three_bedroom, penthouse, loft, 
+                  studio_size, one_bedroom_size, two_bedroom_size, three_bedroom_size, penthouse_size, loft_size, 
+                  studio_price, one_bedroom_price, two_bedroom_price, three_bedroom_price, penthouse_price, loft_price,
+                  studio_monthly, one_bedroom_monthly, two_bedroom_monthly, three_bedroom_monthly, penthouse_monthly, loft_monthly",
+                  :conditions => ["hidden = ?", false], :order => "name ASC",
+                  :include => :developer)
   end
   
   def self.show_all_featured
-    Property.find(:all, :conditions => ["hidden = ? and featured = ?", false, true], :order => "name ASC")
+    #home_page
+    Property.find(:all, :select => "name, permalink, target_completion_date, developer_id, photo_file_name, photo_content_type, photo_file_size, photo_updated_at, as_low_as_label, as_low_as",
+                  :conditions => ["hidden = ? and featured = ?", false, true], :order => "name ASC",
+                  :include => :developer)
   end
   
   def to_param
