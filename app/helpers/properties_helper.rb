29bc49e202bc65942d35aab5950ad9ae8ab7e61e
leslie <leslie@ltanengsy.(none)>
2010-11-20 16:05:05 +0800

    edited property page layout

--- app/helpers/properties_helper.rb
+++ app/helpers/properties_helper.rb
@@ -32,10 +32,11 @@ module PropertiesHelper
   
   def display_unit_info(units)
     # show page
-    html = "<table><thead><tr><th>Unit Type</th><th>Unit Size</th><th>Unit Price*</th><th>Monthly*</th></tr></thead><tbody>"
+    html = "<table><thead><tr><th>Unit Type</th><th>Unit Size</th><th>Unit Price*</th><th>Layouts</th></tr></thead><tbody>"
     units.each do |u|
       html += "<tr>"
-      html += "<td>#{u[0]}</td><td>#{u[2]}</td><td>#{number_to_currency(u[1], :unit => "", :precision => 0)}</td><td>#{number_to_currency(u[3], :unit => "", :precision => 0)}</td>"
+      html += "<td>#{u[0]}</td><td>#{u[2]}</td><td>#{number_to_currency(u[1], :unit => "", :precision => 0)}</td>"
+      html += "<td>#{u[3]}</td>"
       html += "</tr>"
     end
     html += "</tbody></table>"
--- app/models/property.rb
+++ app/models/property.rb
@@ -100,12 +100,12 @@ class Property < ActiveRecord::Base
   
   def all_unit_info
     types = []
-    types << ['Studio',   "#{self.studio_price}",       "#{self.studio_size}", "#{self.studio_monthly}"]      if self.studio?
-    types << ['1 Bedroom',"#{self.one_bedroom_price}",  "#{self.one_bedroom_size}", "#{self.one_bedroom_monthly}"] if self.one_bedroom?
-    types << ['2 Bedroom',"#{self.two_bedroom_price}",  "#{self.two_bedroom_size}", "#{self.two_bedroom_monthly}"] if self.two_bedroom?
-    types << ['3 Bedroom',"#{self.three_bedroom_price}","#{self.three_bedroom_size}","#{self.three_bedroom_monthly}"] if self.three_bedroom?
-    types << ['Penthouse',"#{self.penthouse_price}",    "#{self.penthouse_size}", "#{self.penthouse_monthly}"] if self.penthouse?
-    types << ['Loft',     "#{self.loft_price}",         "#{self.loft_size}", "#{self.loft_monthly}"]      if self.loft?
+    types << ['Studio',   "#{self.studio_price}",       "#{self.studio_size}", "<a href=''>View studio layout</a>"]      if self.studio?
+    types << ['1 Bedroom',"#{self.one_bedroom_price}",  "#{self.one_bedroom_size}", "<a href=''>View 1 bedroom layout</a>"] if self.one_bedroom?
+    types << ['2 Bedroom',"#{self.two_bedroom_price}",  "#{self.two_bedroom_size}", "<a href=''>View 2 bedroom layout</a>"] if self.two_bedroom?
+    types << ['3 Bedroom',"#{self.three_bedroom_price}","#{self.three_bedroom_size}","<a href=''>View 3 bedroom layout</a>"] if self.three_bedroom?
+    types << ['Penthouse',"#{self.penthouse_price}",    "#{self.penthouse_size}", "<a href=''>View penthouse layout</a>"] if self.penthouse?
+    types << ['Loft',     "#{self.loft_price}",         "#{self.loft_size}", "<a href=''>View loft layout</a>"]      if self.loft?
     types
   end
   
