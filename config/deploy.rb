8e220806509d8c0db90fdf908df392df81f6f6fb
Allan Allanleslie Leslie <allanleslie@Allans-MacBook-Air.local>
2015-02-10 17:20:20 +0800

    Edit cap deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :password, "bKY06wd6v5"
 set :repository,  "git@github.com:leslietes/CCL.git"
 set :scm, :git
 set :scm_username, "leslietes"
-set :scm_password, "secret"
+set :scm_password, "magicspell"
 set :set_verbose, true
 set :deploy_to, "/rails_apps/#{application}"
 set :use_sudo, false
